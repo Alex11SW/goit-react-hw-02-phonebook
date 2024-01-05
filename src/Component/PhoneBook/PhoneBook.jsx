b7ebd531bbc3fa984f06be85986a737cee580e7d
alex_shklyar <136641877+Alex11SW@users.noreply.github.com>
2024-01-05 14:33:37 +0200

    new

---
+++ src/Component/PhoneBook/PhoneBook.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import styles from "./phonebook.module.css";
+
+class PhoneBook extends Component {
+    render() {
+        return (
+            <div className={styles.books}>
+                <h1>Hello!!!</h1>
+            </div>
+        )
+    }
+}
+
+export default PhoneBook;
